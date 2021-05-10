9ac5de906ab58161a1b83b52e0fedfa047e2178d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-05-10 23:15:29 +0900

    feat: add meta data search

--- src/app/src/main/java/kaleidot725/exifreader/ui/metadata/MetadataFragment.kt
+++ src/app/src/main/java/kaleidot725/exifreader/ui/metadata/MetadataFragment.kt
@@ -1,13 +1,18 @@
 package kaleidot725.exifreader.ui.metadata
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kaleidot725.exifreader.R
 import kaleidot725.exifreader.databinding.MetadataFragmentBinding
 import kaleidot725.exifreader.extention.dataBinding
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class MetadataFragment : Fragment(R.layout.metadata_fragment) {
     private val viewModel: MetadataViewModel by viewModel()
@@ -36,6 +41,15 @@ class MetadataFragment : Fragment(R.layout.metadata_fragment) {
             this.layoutManager = LinearLayoutManager(context)
             this.setHasFixedSize(true)
         }
+        binding.searchView.addTextChangedListener { editable ->
+            val text = editable.toString()
+            viewModel.updateSearchText(text)
+            if (text.isEmpty()) viewModel.load(path)
+        }
+        binding.searchButton.setOnClickListener {
+            viewModel.load(path)
+            hideSoftKeyboard(activity)
+        }
     }
 
     private fun setupObserve() {
@@ -43,5 +57,13 @@ class MetadataFragment : Fragment(R.layout.metadata_fragment) {
         viewModel.metas.observe(viewLifecycleOwner) {
             metadataAdapter.update(it)
         }
+    }
+
+    private fun hideSoftKeyboard(activity: FragmentActivity?) {
+        if (activity?.currentFocus == null) {
+            return
+        }
+        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(activity.currentFocus!!.windowToken, 0)
     }
 }
--- src/app/src/main/java/kaleidot725/exifreader/ui/metadata/MetadataViewModel.kt
+++ src/app/src/main/java/kaleidot725/exifreader/ui/metadata/MetadataViewModel.kt
@@ -1,10 +1,7 @@
 package kaleidot725.exifreader.ui.metadata
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kaleidot725.exifreader.data.Metadata
 import kaleidot725.exifreader.repository.PictureRepository
 import kotlinx.coroutines.launch
@@ -12,17 +9,30 @@ import kotlinx.coroutines.launch
 class MetadataViewModel(private val repository: PictureRepository) : ViewModel() {
     private val _metas: MutableLiveData<List<Metadata>> = MutableLiveData()
     val metas: LiveData<List<Metadata>> = _metas
+    val notFoundMetas: LiveData<Boolean> = metas.map { it.isEmpty() }
+
+    private val _searchText: MutableLiveData<String> = MutableLiveData("")
+    val searchText: LiveData<String> = _searchText
 
     fun load(path: String) {
         viewModelScope.launch {
             try {
                 val picture = repository.getByPath(path) ?: return@launch
-                val metadataList = repository.getMetadata(picture) ?: return@launch
-                _metas.postValue(metadataList)
+                val searchText = _searchText.value ?: ""
+                val metadataList = repository.getMetadata(picture)
+                val filteredMetadataList = if (searchText.isNotEmpty()) metadataList.filter { metadata ->
+                    val regex = Regex(searchText)
+                    regex.containsMatchIn(metadata.name)
+                } else metadataList
+
+                _metas.postValue(filteredMetadataList)
             } catch (e: Exception) {
-                // FIXME
                 Log.e("MedatadataViewModel", e.toString())
             }
         }
+    }
+
+    fun updateSearchText(text: String) {
+        _searchText.value = text
     }
 }
